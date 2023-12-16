1f5a8396d555d9f539458f1b0310c8ba2d8a22b4
DRV111 <xblackcrownx@gmail.com>
2023-12-16 14:27:23 +0300

    feat: add CityList and CityItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './index.css';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import HomePage from './pages/HomePage';
 import NotFound from './pages/NotFound';
 import Login from './pages/Login';
 import AppLayout from './pages/AppLayout';
-import './index.css';
+import CityList from './components/CityList';
+import { useState, useEffect } from 'react';
+
+const DATA_URL = 'http://localhost:9000';
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${DATA_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert('Error fetching cities');
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -16,8 +39,14 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>LIST</p>} />
-          <Route path="cities" element={<p>List of cities</p>} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>List of countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
