5ead18284aa503c837fd8e5ab5b62b57127cf4d9
DRV111 <xblackcrownx@gmail.com>
2023-12-16 10:53:05 +0300

    feat: add Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import HomePage from './pages/HomePage';
 import NotFound from './pages/NotFound';
-import './index.css';
 import Login from './pages/Login';
-// import AppLayout from './pages/AppLayout';
+import AppLayout from './pages/AppLayout';
+import './index.css';
 
 function App() {
   return (
@@ -14,9 +14,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="login" element={<Login />} />
-        {/* <Route path="app" element={<AppLayout />} /> */}
-
+        <Route path="/login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
