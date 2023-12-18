ceec20abc731a37b4357c37834d04413a2a7face
DRV111 <xblackcrownx@gmail.com>
2023-12-18 12:51:20 +0300

    feat: get latitude and longitude with useSearchParams hook

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,18 @@
+import { useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 
 function Map() {
-  return <div className={styles.mapContainer}></div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <p>lat: {lat}</p>
+      <p>lng: {lng}</p>
+      <button onClick={}>Change Position</button>
+    </div>
+  );
 }
 
 export default Map;
