3d00fea77d9bfe92d9a41cce368b8e8016588476
DRV111 <xblackcrownx@gmail.com>
2023-12-18 13:22:13 +0300

    feat: add navigation in Form component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,16 +1,19 @@
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 
 function Map() {
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
   return (
-    <div className={styles.mapContainer}>
+    <div className={styles.mapContainer} onClick={() => navigate('form')}>
       <h1>Map</h1>
       <p>lat: {lat}</p>
       <p>lng: {lng}</p>
-      <button onClick={}>Change Position</button>
+      <button onClick={() => setSearchParams({ lat: 20, lng: 50 })}>
+        Change Position
+      </button>
     </div>
   );
 }
