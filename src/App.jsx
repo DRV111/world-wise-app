f46b05c8a35b025f2afd846bd32ee0e00e576d46
DRV111 <xblackcrownx@gmail.com>
2023-12-18 13:27:04 +0300

    fix: set countries tab active by default

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import './index.css';
 import Product from './pages/Product';
@@ -43,10 +43,7 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate to={'cities'} />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
