c807df32cbc53e48ed7eead18d6132efef5a5eac
DRV111 <xblackcrownx@gmail.com>
2024-01-13 10:04:03 +0300

    fix: fix infinite request bug

--- src/context/CitiesContext.jsx
+++ src/context/CitiesContext.jsx
@@ -1,4 +1,10 @@
-import { useEffect, createContext, useContext, useReducer } from 'react';
+import {
+  useEffect,
+  createContext,
+  useContext,
+  useReducer,
+  useCallback,
+} from 'react';
 
 const DATA_URL = 'http://localhost:9000';
 
@@ -84,17 +90,20 @@ function CitiesContextProvider({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
-    if (Number(id) === currentCity.id) return;
-    dispatch({ type: 'loading' });
-    try {
-      const res = await fetch(`${DATA_URL}/cities/${id}`);
-      const data = await res.json();
-      dispatch({ type: 'city/loaded', payload: data });
-    } catch {
-      dispatch({ type: 'rejected', payload: 'Error fetching city' });
-    }
-  }
+  const getCity = useCallback(
+    async function getCity(id) {
+      if (Number(id) === currentCity.id) return;
+      dispatch({ type: 'loading' });
+      try {
+        const res = await fetch(`${DATA_URL}/cities/${id}`);
+        const data = await res.json();
+        dispatch({ type: 'city/loaded', payload: data });
+      } catch {
+        dispatch({ type: 'rejected', payload: 'Error fetching city' });
+      }
+    },
+    [currentCity.id]
+  );
 
   async function createCity(newCity) {
     dispatch({ type: 'loading' });
